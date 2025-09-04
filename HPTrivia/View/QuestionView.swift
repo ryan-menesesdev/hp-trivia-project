10ab50ddff8c22154b09f01b82a84711f15276d6
Ryan Meneses <ryanmeneses.dev@gmail.com>
2025-09-04 15:09:15 -0300

    add: animations and transitions to Hint and Book reveals

--- HPTrivia/View/QuestionView.swift
+++ HPTrivia/View/QuestionView.swift
@@ -7,8 +7,9 @@ struct QuestionView: View {
     
     @State private var musicPlayer: AVAudioPlayer!
     @State private var sfxPlayer: AVAudioPlayer!
-    
     @State private var animateViewsIn = false
+    @State private var revealHint = false
+    @State private var revealBook = false
     
     var body: some View {
         GeometryReader { geo in
@@ -55,29 +56,100 @@ struct QuestionView: View {
                     }
                     .animation(.easeInOut(duration: 0.5), value: animateViewsIn)
                     
-                    Spacer()
-                    
                     HStack {
-                        Button {
-                            
-                        } label: {
-                            Image(systemName: "questionmark.app.fill")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .imageScale(.large)
-                                
-                        }
+                        // Hints
                         
-                        Button {
-                            
-                        } label: {
-                            Image(systemName: "questionmark.app.fill")
-                                .foregroundStyle(.blue)
-                                .font(.largeTitle)
-                                .imageScale(.large)
-                                
+                        VStack {
+                            if animateViewsIn {
+                                Button {
+                                    withAnimation(.easeOut(duration: 0.75)) {
+                                        revealHint.toggle()
+                                    }
+                                    playFlip()
+                                    game.correctAnswerScore -= 1
+                                } label: {
+                                    Image(systemName: "questionmark.app.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100)
+                                        .foregroundStyle(.cyan)
+                                        .padding()
+                                        .phaseAnimator([false, true]) { content, phase in
+                                            content
+                                                .rotationEffect(.degrees(phase ? -13 : -17))
+                                        } animation: { _ in
+                                                .easeInOut(duration: 0.5)
+                                        }
+                                        .rotation3DEffect(.degrees(revealHint ? 1440 : 0), axis: (x: 0, y: 1, z: 0))
+                                        .scaleEffect(revealHint ? 5 : 1)
+                                        .offset(x: revealHint ? geo.size.width/2 : 0)
+                                        .opacity(revealHint ? 0 : 1)
+                                        .overlay {
+                                            Text("\(game.currentQuestion.hint)")
+                                                .padding(.leading, 20)
+                                                .minimumScaleFactor(0.5)
+                                                .multilineTextAlignment(.center)
+                                                .opacity(revealHint ? 1 : 0)
+                                                .scaleEffect(revealHint ? 1.33 : 1)
+                                        }
+                                }
+                                .transition(.offset(x: -geo.size.width/2))
+                            }
                         }
+                        .animation(.easeIn(duration: 1.5).delay(1), value: animateViewsIn)
+
+                        Spacer()
+                        
+                        VStack {
+                            if animateViewsIn {
+                                Button {
+                                    withAnimation(.easeOut(duration: 0.75)) {
+                                        revealBook.toggle()
+                                    }
+                                    playFlip()
+                                    game.correctAnswerScore -= 1
+                                } label: {
+                                    Image(systemName: "app.fill")
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(width: 100)
+                                        .foregroundStyle(.cyan)
+                                        .overlay(content: {
+                                            Image(systemName: "book.closed")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .frame(width: 50)
+                                                .foregroundStyle(.black)
+                                        })
+                                        .padding()
+                                        .phaseAnimator([false, true]) { content, phase in
+                                            content
+                                                .rotationEffect(.degrees(phase ? 13 : 17))
+                                        } animation: { _ in
+                                                .easeInOut(duration: 0.5)
+                                        }
+                                        .rotation3DEffect(.degrees(revealBook ? -1440 : 0), axis: (x: 0, y: 1, z: 0))
+                                        .scaleEffect(revealBook ? 5 : 1)
+                                        .offset(x: revealBook ? -geo.size.width/2 : 0)
+                                        .opacity(revealBook ? 0 : 1)
+                                        .overlay {
+                                            Image("hp\(game.currentQuestion.book)")
+                                                .resizable()
+                                                .scaledToFit()
+                                                .padding(.trailing, 20)
+                                                .opacity(revealBook ? 1 : 0)
+                                                .scaleEffect(revealBook ? 1.33 : 1)
+                                            
+                                        }
+                                }
+                                .transition(.offset(x: geo.size.width/2))
+                            }
+                        }
+                        .animation(.easeIn(duration: 1.5).delay(1), value: animateViewsIn)
                     }
+                    .padding()
+                    
+                    Spacer()
                 }
                 .frame(width: geo.size.width, height: geo.size.height)
                     
@@ -93,7 +165,7 @@ struct QuestionView: View {
             }
             
             DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-                playMusic()
+//                playMusic()
             }
         }
     }
